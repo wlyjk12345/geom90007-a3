4bbf1f5c4ace744d7b3bdebedc5ca3ad4625fdee
xianruig <71248879+xianruig@users.noreply.github.com>
2023-10-16 21:07:40 +1100

    Update main.R

--- main.R
+++ main.R
@@ -1,13 +1,10 @@
 # App dependencies-------------------------------------------------------------
 # Please run the following to ensure dependencies are installed
 source("./R/libraries.R")
-
-# Data-------------------------------------------------------------------------
 source("./R/data.R")
-
-# Mapping----------------------------------------------------------------------
-source("./R/map.R")
-
+source("./R/icon.R")
+source("./R/ui.R")
+source("./R/server.R")
 
 # Define the Shiny app
-shiny::shinyApp(ui, server)
+shinyApp(ui, server)
