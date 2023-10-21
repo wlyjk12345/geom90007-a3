24089d01f23934d1b90eeffbb3cf9632d1740b48
YangJK <wlyjk123456@gmail.com>
2023-10-21 16:50:44 +1100

    add inf in tour_panel

--- new_R/ui.R
+++ new_R/ui.R
@@ -40,76 +40,106 @@ intro_panel <- tabPanel(
     fluidRow(
       includeHTML("home.html"),
     ),
+    fluidRow(
+      column(12, 
+             tableauPublicViz(
+               id = "tableauviz1",
+               url = "https://public.tableau.com/views/Pedestrain_Map/1_1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+             )
+      )
+    ),
+    tags$hr(),
+    tags$h5(class = "footer-note",'Data Source from: ', 
+            a("CoM Open Data Portal - City of Melbourne",
+              href="https://data.melbourne.vic.gov.au/pages/home/")),
+    tags$h5(class = "footer-note",'Tableau map deposit: ', 
+            a("Xianrui Gao's Pedestrain_Map",
+              href="https://public.tableau.com/app/profile/xianrui.gao/viz/Pedestrain_Map/1_1"))
   )
 )
 
 faqs_panel <- tabPanel(
   title = "faqs",
   class = "page-1",
-    fluidPage(
+  fluidPage(
     fluidRow(
       includeHTML("faqs.html"),
-  ),
-)
+    ),
+  )
 )
 
 poi_panel <- tabPanel("poi",
-            fluidPage(
-              tags$h1(class = "shiny-title","Melbourne Dining and Accommodation"),  # Add the title here
-              tags$hr(),
-              tags$p(class = "shiny-p","Explore Melbourne with the Points of Interest Map. Discover cafes, restaurants, pubs, accommodation, and more. 
+                      fluidPage(
+                        tags$h1(class = "shiny-title","Melbourne Dining and Accommodation"),  # Add the title here
+                        tags$hr(),
+                        tags$p(class = "shiny-p","Explore Melbourne with the Points of Interest Map. Discover cafes, restaurants, pubs, accommodation, and more. 
                      Filter your search and find your favorites with ease."),  # Add the description here        
-    
-               sidebarLayout(
-                 sidebarPanel(
-                   h3("Search "),
-                   textInput("geocode", "Type the name", placeholder = "e.g., McDonald"),
-                   actionButton("go", "Search!"),
-                   br(),
-                   checkboxGroupInput("filter", "Filter by category:",
-                                      choices = c("Cafes and Restaurants", "Takeaway Food Services", 
-                                                  "Pubs, Taverns and Bars", "Accommodation", 
-                                                  "Bakery Product Manufacturing (Non-factory based)"),
-                                      selected = c("Cafes and Restaurants", "Takeaway Food Services", 
-                                                   "Pubs, Taverns and Bars", "Accommodation", 
-                                                   "Bakery Product Manufacturing (Non-factory based)")),
-                   helpText("Use the search bar to find facility in Melbourne.")
-                 ),
-                 mainPanel(
-                   leafletOutput("map", height = 1000, width = 600)
-                 )
-               ),
-              tags$hr(),
-              tags$h5(class = "footer-note",'Data Source from: ', 
-                      a("CoM Open Data Portal - City of Melbourne",
-                        href="https://data.melbourne.vic.gov.au/pages/home/"))
-            )
+                        
+                        sidebarLayout(
+                          sidebarPanel(
+                            h3("Search "),
+                            textInput("geocode", "Type the name", placeholder = "e.g., McDonald"),
+                            actionButton("go", "Search!"),
+                            br(),
+                            checkboxGroupInput("filter", "Filter by category:",
+                                               choices = c("Cafes and Restaurants", "Takeaway Food Services", 
+                                                           "Pubs, Taverns and Bars", "Accommodation", 
+                                                           "Bakery Product Manufacturing (Non-factory based)"),
+                                               selected = c("Cafes and Restaurants", "Takeaway Food Services", 
+                                                            "Pubs, Taverns and Bars", "Accommodation", 
+                                                            "Bakery Product Manufacturing (Non-factory based)")),
+                            helpText("Use the search bar to find facility in Melbourne.")
+                          ),
+                          mainPanel(
+                            leafletOutput("map", height = 1000, width = 600)
+                          )
+                        ),
+                        tags$hr(),
+                        tags$h5(class = "footer-note",'Data Source from: ', 
+                                a("CoM Open Data Portal - City of Melbourne",
+                                  href="https://data.melbourne.vic.gov.au/pages/home/"))
+                      )
 )
 
 tour_panel <- tabPanel("tour", 
-            fluidPage(
-              tags$h1(class = "shiny-title","Tourist Amenities Map"),  # Add the title here
-              tags$hr(),
-              tags$p(class = "shiny-p","Discover essential tourist amenities at your fingertips with our Tourist Amenities Map. Find Wi-Fi hotspots, tram stops, bus stops, 
+                       fluidPage(
+                         tags$h1(class = "shiny-title","Tourist Amenities Map"),  # Add the title here
+                         tags$hr(),
+                         tags$p(class = "shiny-p","Discover essential tourist amenities at your fingertips with our Tourist Amenities Map. Find Wi-Fi hotspots, tram stops, bus stops, 
                     and more to make your visit to Melbourne convenient and enjoyable. Plan your journey and explore the city with ease, thanks to this handy map."),  # Add the description here        
                          
-               sidebarLayout(
-                 sidebarPanel(
-                   h3("Filter Local Facilities"),
-                   checkboxGroupInput("newTabFilter", "Filter by type:",
-                                      choices = c("WiFi", "Tram Stops", "Bus Stops", "Bike Share Docks", "Routes"),
-                                      selected = c("WiFi", "Tram Stops", "Bus Stops", "Bike Share Docks", "Routes")),
-                   helpText("Filter the local facilities shown on the map.")
-                 ),
-                 mainPanel(
-                   leafletOutput("newTabMap", height = 1000, width = 600)
-                 )
-                ),
-                tags$hr(),
-                tags$h5(class = "footer-note",'Data Source from: ', 
-                    a("CoM Open Data Portal - City of Melbourne",
-                      href="https://data.melbourne.vic.gov.au/pages/home/"))
-            )
+                         sidebarLayout(
+                           sidebarPanel(
+                             h3("Filter Local Facilities"),
+                             checkboxGroupInput("newTabFilter", "Filter by type:",
+                                                choices = c("WiFi", "Tram Stops", "Bus Stops", "Bike Share Docks", "Routes"),
+                                                selected = c("WiFi", "Tram Stops", "Bus Stops", "Bike Share Docks", "Routes")),
+                             helpText("Filter the local facilities shown on the map."),
+                             
+                             # Add links with the shiny-p class
+                             hr(),
+                             a("About Melbourne WiFi", href = "https://www.visitvictoria.com/practical-information/wifi-hotspots", class = "shiny-p"),
+                             hr(),
+                             a("About Tram Stops", href = "https://www.ptv.vic.gov.au/assets/PTV-default-site/Maps-and-Timetables-PDFs/Maps/Network-maps/Victorian-Train-Network-Map-May-2023-v3.pdf.pdf", class = "shiny-p"),
+                             hr(),
+                             a("About Bus Stops", href = "https://citymapper.com/melbourne/bus/stops?name=&coords=-37.817237%2C144.967974", class = "shiny-p"),
+                             hr(),
+                             a("About Bike Share Docks", href = "https://melbournebikeshare.com.au/", class = "shiny-p"),
+                             hr(),
+                             a("About Routes", href = "https://www.ptv.vic.gov.au/more/maps/", class = "shiny-p"),
+                             
+                             
+                             # Add more links as needed
+                           ),
+                           mainPanel(
+                             leafletOutput("newTabMap", height = 1000, width = 600)
+                           )
+                         ),
+                         tags$hr(),
+                         tags$h5(class = "footer-note",'Data Source from: ', 
+                                 a("CoM Open Data Portal - City of Melbourne",
+                                   href="https://data.melbourne.vic.gov.au/pages/home/"))
+                       )
 )
 
 
@@ -160,40 +190,40 @@ weather_panel <- tabItem("weather",
 
 
 pedestrain_panel <- tabPanel("pedestrain",
-                         fluidPage(
-                         tags$h1(class = "shiny-title","Pedestrian Density Map"),  # Add the title here
-                         tags$hr(),
-                         tags$p(class = "shiny-p","Explore Melbourne's lively foot traffic with our Pedestrian Density Map. 
+                             fluidPage(
+                               tags$h1(class = "shiny-title","Pedestrian Density Map"),  # Add the title here
+                               tags$hr(),
+                               tags$p(class = "shiny-p","Explore Melbourne's lively foot traffic with our Pedestrian Density Map. 
                                 Uncover popular streets and bustling walkways, perfect for strolling through iconic neighborhoods. Gain valuable insights into the city's vibrant, walkable environment and discover Melbourne on foot with ease."),  # Add the description here        
-                         
-                         
-                           #h2("Pedestrian Density Map",
-                           #   align = "center"),
-                           fluidRow(
-                               column(12, 
-                                    tableauPublicViz(
-                                      id = "tableauviz1",
-                                      url = "https://public.tableau.com/views/Pedestrain_Map/1_1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
-                              )
-                              )
-                             ),
-                           #h2("Weather Forecast of Melbourne", align = "center"),
-                           # hr(),
-                           # fluidRow(
-                           #   column(12,
-                           #   tableauPublicViz(
-                           #     id = "tableauviz2",
-                           #     url = "https://public.tableau.com/views/Pedestrain_Map/1_1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
-                           #   )
-                           # )),
-                         tags$hr(),
-                         tags$h5(class = "footer-note",'Data Source from: ', 
-                                 a("CoM Open Data Portal - City of Melbourne",
-                                   href="https://data.melbourne.vic.gov.au/pages/home/")),
-                         tags$h5(class = "footer-note",'Tableau map deposit: ', 
-                                 a("Xianrui Gao's Pedestrain_Map",
-                                   href="https://public.tableau.com/app/profile/xianrui.gao/viz/Pedestrain_Map/1_1"))
-                         )
+                               
+                               
+                               #h2("Pedestrian Density Map",
+                               #   align = "center"),
+                               fluidRow(
+                                 column(12, 
+                                        tableauPublicViz(
+                                          id = "tableauviz1",
+                                          url = "https://public.tableau.com/views/Pedestrain_Map/1_1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+                                        )
+                                 )
+                               ),
+                               #h2("Weather Forecast of Melbourne", align = "center"),
+                               # hr(),
+                               # fluidRow(
+                               #   column(12,
+                               #   tableauPublicViz(
+                               #     id = "tableauviz2",
+                               #     url = "https://public.tableau.com/views/Pedestrain_Map/1_1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+                               #   )
+                               # )),
+                               tags$hr(),
+                               tags$h5(class = "footer-note",'Data Source from: ', 
+                                       a("CoM Open Data Portal - City of Melbourne",
+                                         href="https://data.melbourne.vic.gov.au/pages/home/")),
+                               tags$h5(class = "footer-note",'Tableau map deposit: ', 
+                                       a("Xianrui Gao's Pedestrain_Map",
+                                         href="https://public.tableau.com/app/profile/xianrui.gao/viz/Pedestrain_Map/1_1"))
+                             )
 )
 
 ui <- dashboardPage(
@@ -225,4 +255,5 @@ ui <- dashboardPage(
     )
   )
 )
+
 
